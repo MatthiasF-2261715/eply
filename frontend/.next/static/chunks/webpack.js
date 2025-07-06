c9d33321bb5f79e6e9be5bebb9ac7f16e7bb1cbd
Matthias Foesters <matthias.foesters@student.uhasselt.be>
2025-07-06 17:02:03 +0200

    safe push

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4ea4049f8d974a47"; }
+/******/ 		__webpack_require__.h = function() { return "bcff97ddf07c272f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
