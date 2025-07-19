73d59281ae1c594c6aaaead062be2de08a17125e
Matthias Foesters <matthias.foesters@student.uhasselt.be>
2025-07-19 15:22:58 +0200

    add assistants

--- frontend/.next/server/webpack-runtime.js
+++ frontend/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("64aad0375722264e")
+/******/ 		__webpack_require__.h = () => ("7524e45954646ef6")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
