0b60ead9c8fe2ff7aa3132845966d6045342a24d
Matthias Foesters <matthias.foesters@student.uhasselt.be>
2025-08-21 13:58:03 +0200

    added better handling

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,14 +1,8 @@
 function isAuthenticated(req, res, next) {
-    console.log("Checking session " + req.session);
+    console.log("Checking session " + req.session.isAuthenticated);
         if (!req.session.isAuthenticated) {
         return res.redirect('/auth/outlook-login');
     }
-    
-    // Add this to ensure session is saved
-    req.session.save((err) => {
-        if (err) console.error('Session save error:', err);
-        next();
-    });
 }
 
 module.exports = { isAuthenticated };
