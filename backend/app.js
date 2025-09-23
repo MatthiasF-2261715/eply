c1914b800f75b88449024c7fc1b28a4a0da00d6d
Matthias Foesters <matthias.foesters@student.uhasselt.be>
2025-09-23 14:51:41 +0200

    increased limit backend

--- backend/app.js
+++ backend/app.js
@@ -54,9 +54,9 @@ app.use(session({
 }));
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
